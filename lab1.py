04254ee1865920ad50debe7dd2fe9462893fa4e4
Rie Wilsterman <riewilsterman@mac.com>
2026-03-26 17:24:31 -0700

    Update lab1 with exercises 1-7

--- lab1.py
+++ lab1.py
@@ -1,3 +1,5 @@
+#1
+
 a = 10
 b = 19
 
@@ -9,4 +11,32 @@ print(type(b))
 
 print(id(a))
 print(id(b))
+
+#2
+
+print('the quick brown fox')
+print("the quick brown fox")
+print("""the quick brown fox""")
+
+# #3
+
+my_string = "the quick brown fox"
+# #4
+print(my_string[0:5])   # first 5 characters
+print(my_string[-1])    # last character
+
+
+# #5
+print(len(my_string))   # length of string
+
+
+# #6
+print(my_string.upper())   # uppercase
+print(my_string.lower())   # lowercase
+
+
+# #7
+print(my_string.replace("fox", "cat"))
+print(my_string)
+print(f"My string is: {my_string}")
 
