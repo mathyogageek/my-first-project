384f81e2e16e3eac9199d1fc0f437ce0c03ae3f6
Rie Wilsterman <riewilsterman@mac.com>
2026-04-04 15:54:37 -0700

    update lab1

--- lab1.py
+++ lab1.py
@@ -1,5 +1,4 @@
 #1
-
 a = 10
 b = 19
 
@@ -18,19 +17,18 @@ print('the quick brown fox')
 print("the quick brown fox")
 print("""the quick brown fox""")
 
-# #3
-
+#3
 my_string = "the quick brown fox"
-# #4
+
+print(f"My sentence is: {my_string}")
+
+#4
 print(my_string[0:5])   # first 5 characters
 print(my_string[-1])    # last character
 
-
-# #5
+#5
 print(5**2 + 34 > 6 + 8/2*34)
-
-
-# #6
+#6
 
 weather = "warm"
 
@@ -39,7 +37,6 @@ if weather == "warm":
 else:
     print("I'll stay home")
 
+#7
 
-# #7
-# Fix expression to get 22
 print((4 + 6*(9-3)/2))
